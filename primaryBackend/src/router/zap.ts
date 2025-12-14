571e5c30c65e9d4d6de288be576859a65e6bd072
komal-kumawat <kumawatkomal17122004@gmail.com>
2025-12-15 01:52:57 +0530

    zap router logic done

--- primaryBackend/src/router/zap.ts
+++ primaryBackend/src/router/zap.ts
@@ -1,10 +1,25 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/Authmiddleware.js";
+import { ZapCreateSchema } from "../models/user.js";
+import { prismaclient } from "../db/index.js";
 
 const router = Router();
 
-router.post("/" , authMiddleware,(req , res)=>{
-    console.log("create a zap");
+// create a zap
+router.post("/" , authMiddleware, async(req , res)=>{
+    // @ts-ignore
+    const id:string = req.id;
+    const body = req.body;
+    const parsedData = ZapCreateSchema.safeParse(body);
+
+    if(!parsedData.success){
+        return res.status(409).json({
+            message:"incorrect inputs"
+        })
+    };
+
+    const zapId = await prismaclient.$transaction()
+
 })
 router.get("/" ,authMiddleware ,  (req , res)=>{
     console.log("zap");
