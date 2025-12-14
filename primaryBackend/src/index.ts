1c1664d8bc326cb52de8b4adba6a3f138ab5f6a0
komal-kumawat <kumawatkomal17122004@gmail.com>
2025-12-15 01:59:12 +0530

    router logic for action and trigger done

--- primaryBackend/src/index.ts
+++ primaryBackend/src/index.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import { userRouter } from "./router/user.js";
+import cors from "cors";
+import { zapRouter } from "./router/zap.js";
+import { triggerRouter } from "./router/trigger.js";
+import { actionRouter } from "./router/action.js";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.use("api/v1/user" , userRouter);
 app.use("api/v1/zap" , zapRouter);
+app.use("api/v1.trigger" , triggerRouter);
+app.use("api/v1/action" , actionRouter);
 
 const port = 8000;
 app.listen(port , ()=>{
---
+++ primaryBackend/src/router/action.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { prismaclient } from "../db/index.js";
+
+const router = Router();
+router.get("/available" , async(req , res)=>{
+    const availableActions = await prismaclient.availableAction.findMany({});
+    res.json({availableActions});
+});
+
+export const actionRouter = router;
